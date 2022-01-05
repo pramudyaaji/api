df1646bc868abc406738564aa041e6265819f534
Pramudya Aji <76149321+pramudyaaji@users.noreply.github.com>
2022-01-05 13:41:57 +0700

    Add files via upload

---
+++ api_tampil_all.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../koneksi.php');
+$myArray = Array();
+if($result = mysqli_query($conn, "SELECT * FROM produk ORDER BY id ASC")){
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+        $myArray[] = $row;
+    }
+    mysqli_close($conn);
+    echo json_encode($myArray);
+}
+?>
